6de68a7bae55d4b498ec2933f71d64001f21e40b
cocode <seeucoco@163.com>
2022-10-19 14:56:05 +0800

    add readme.md

--- README.md
+++ README.md
@@ -1,11 +1,39 @@
 ## Pretty Printer
 
+### Sample
+
+```java
+// string:
+Pretty.print("Hi there! This is Pretty-Printer!😉");
+// boolean:
+Pretty.print(false);
+Pretty.print(true);
+// number:
+Pretty.print(1);
+// null:
+Pretty.print(null);
+// list:
+Pretty.print(Arrays.asList(1, 2, 3, 4, 5, 6));
+// object:
+User tom = new User("Tom", "https://io.youxingz/images/avatar_a.png", 17, 1.82f, "How is it going");
+User john = new User("John", "https://io.youxingz/images/avatar_b.png", 18, 1.76f, "I am fine, thank you");
+Friend friend = new Friend(tom, john);
+Pretty.print(friend);
+// list of objects:
+Pretty.print(Arrays.asList(tom, tom, tom));
+```
+Console output:
+
+![img.png](img.png)
+
 ### Installation
 
-#### Maven
+#### [Maven Dependency](https://github.com/youxingz/pretty-printer/packages/1679687)
 
 ```xml
 <dependency>
-    ?
+    <groupId>io.pretty</groupId>
+    <artifactId>pretty-printer</artifactId>
+    <version>1.0-SNAPSHOT</version>
 </dependency>
 ```
--- src/test/java/io/pretty/PrettyTest.java
+++ src/test/java/io/pretty/PrettyTest.java
@@ -9,16 +9,23 @@ import java.util.Arrays;
 public class PrettyTest {
     @Test
     public void test1() {
-        User tom = new User("Tom", "https://io.xx/images/avatar_123.png", 17, 1.82f, "How is it going");
-        User john = new User("John", "https://io.xx/images/avatar_123.png", 18, 1.76f, "I am fine, thank you");
-        Friend friend = new Friend(tom, john);
-        Pretty.print(friend);
-        Pretty.print("demo");
+        // string:
+        Pretty.print("Hi there! This is Pretty-Printer!😉");
+        // boolean:
         Pretty.print(false);
         Pretty.print(true);
+        // number:
         Pretty.print(1);
+        // null:
         Pretty.print(null);
-        Pretty.print(Arrays.asList(1, 2, 3));
+        // list:
+        Pretty.print(Arrays.asList(1, 2, 3, 4, 5, 6));
+        // object:
+        User tom = new User("Tom", "https://io.youxingz/images/avatar_a.png", 17, 1.82f, "How is it going");
+        User john = new User("John", "https://io.youxingz/images/avatar_b.png", 18, 1.76f, "I am fine, thank you");
+        Friend friend = new Friend(tom, john);
+        Pretty.print(friend);
+        // list of objects:
         Pretty.print(Arrays.asList(tom, tom, tom));
     }
 }
